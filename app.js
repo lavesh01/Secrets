7c0f447134661623682fb03530d205adb43c2687
lavesh <laveshbisht01@gmail.com>
2022-04-23 12:59:03 +0530

    Add md5 for encrption

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-
-console.log(process.env.SECRET);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,7 +21,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -40,7 +39,7 @@ app.get("/register",function(req,res){
 app.post("/register", function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -54,7 +53,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err,foundUser){
         if(err){
